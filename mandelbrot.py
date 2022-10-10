edfc69428799a4c7230d969e96e93dbe6fa4bd21
Rufluenza <NEXT5177@edu.nextkbh.dk>
2022-10-10 21:01:32 +0200

    Zoom now works, however not as fully intended
    When saving the video it only saves a blank white 3 second mp4 file, I am working on fixing that.

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,11 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
-import subprocess
+import os.path as path
+
+from sys import platform
 
 os.login = os.getlogin()
+if platform == "win32":
+    plt.rcParams['animation.ffmpeg_path'] =(f'C:\\Users\\{os.login}\\Documents\\ffmpeg\\bin\\binffmpeg.exe')
+elif platform == "mac":
+    plt.rcParams['animation.ffmpeg_path'] ='/Users/{os.login}/Documents/ffmpeg'
+elif platform == "linux":
+    plt.rcParams['animation.ffmpeg_path'] ='/usr/bin/ffmpeg'
 
-plt.rcParams['animation.ffmpeg_path'] =(f'C:\\Users\\{os.login}\\Documents\\ffmpeg\\bin\\binffmpeg.exe')
 fig = plt.figure()
 ax = plt.subplots()
 xmin, xmax, ymin, ymax = -1.4, 1.4, -2, 0.8
@@ -18,8 +25,6 @@ def mandelbrot( h,w, maxit=100 ):
     c = x+y*1j # we define the complex number c
     z = c # we define the complex number z
     iteration = maxit + np.zeros(z.shape, dtype=int) # we define the number of iterations
-
-# c = x + y*1j
 
     for i in range(maxit):
         z = z**2 + c
@@ -33,47 +38,32 @@ def mandelbrot( h,w, maxit=100 ):
 # zoom in on the interesting part of the fractal
 # create the zoom effect by changing the coordinates in a loop
 
-# savepath = os.path.dirname('mandelbrot_images')
-
-
-
 # saving the images as a png file but zooming into the fractal ending with a interesting part of the fractal being x = -0.75, y = 0.1, width = 0.005, height = 0.005 but animated so it looks like a zoom effect
 # set saavwepath to the path where you want to save the images default is the current directory
 # use os.mkdir to create a directory to save the images in C:\\Users\\{os.login}\\Documents\\mandelbrot_video
 # first check if the directory exists with os.path.exists
 
+# here we are asking if the user wants to save the images or not
+# and we are also making sure that the user gets the directory right regardless of the operating system
 ask_save = input('Do you want to save the images? (y/n): ')
-if ask_save == 'y':
-    if os.path.exists(f'C:\\Users\\{os.login}\\Documents\\mandelbrot_video'):
-        print('Directory exists')
-    else:
-        os.mkdir(f"C:\\Users\\{os.login}\\Documents\\mandelbrot_video")
-        print('Directory created')
-    savepath = (f"C:\\Users\\{os.login}\\Documents\\mandelbrot_video\\mandelbrot.mp4")
-    
+ask_save = ask_save.capitalize()
 
-    # making the image sequence as ims
-    ims = []
-    for zoom in range(30):
-        # zoom effect
-        if xmin or xmax or ymin or ymax <= 0:
-            break
-        xmin += zoom * 0.00415
-        xmax -= zoom * 0.00585
-        ymin += zoom * 0.00325
-        ymax -= zoom * 0.00675
-        im = ax.imshow(mandelbrot(400,400), animated=True)
-        if zoom == 0:
-            ax.imshow(mandelbrot(400,400))
-        else:
-            ims.append([im])
-    FFwriter = animation.FFMpegWriter()
-    ani = animation.ArtistAnimation(fig, ims, interval=300, blit=True, repeat_delay=1000)
-    ani.save(savepath, writer=FFwriter)
-    print("Done saved the video to the path: ", savepath)
-
-else:
-    plt.imshow(mandelbrot(1000,1000))
+savepath = os.path.abspath("mandelbrot_video.mp4")
+# making the image sequence as ims
+ims = []
+for zoom in range(1, 31):
+    # zoom effect
+    xmin += 0.0415
+    xmax -= 0.0585
+    ymin += 0.0325
+    ymax -= 0.0675
+    im = plt.imshow(mandelbrot(400,400), animated=True)
+    ims.append([im])
+FFwriter = animation.FFMpegWriter(fps=10)
+anim = animation.ArtistAnimation(fig, ims, interval=300, blit=True, repeat_delay=1000)
+if ask_save == 'Y':
     plt.show()
-
-    
+    anim.save("mandelbrot.mp4", writer=FFwriter)
+    print("Done saved the video to the path: ", savepath)
+else:
+    plt.show()
